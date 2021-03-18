43d8d1ddbde736b431521e3472a97b78a2869c61
User <apple@Efe-Atogun.local.tld>
2021-03-18 15:30:35 +0100

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import React from 'react';
 function Footer() {
 	return (
 		<>
-			<footer className="page-footer font-small blue">
+			<footer className="page-footer font-small blue mt-5">
 				<div className="footer-copyright text-center py-3">
 					© 2020 Copyright: Developed by Miracle
 				</div>
